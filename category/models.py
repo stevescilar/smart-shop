59d1d019b63d658448a5f5bff3c9ba14acd2244e
stevescilar <stevemuambi@gmail.com>
2021-12-23 23:26:51 +0300

    oiled

--- category/models.py
+++ category/models.py
@@ -8,5 +8,8 @@ class Category(models.Model):
     description = models.CharField(max_length=255, blank=True) 
     cat_image = models.ImageField(upload_to='photos/categories/',blank = True)
 
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
     def __str__(self):
         return self.category_name
