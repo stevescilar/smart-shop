5416c90057a366a1bb4019d4bda2f304829169c5
stevescilar <stevemuambi@gmail.com>
2021-12-24 01:07:14 +0300

    still burning

--- techit/settings.py
+++ techit/settings.py
@@ -124,7 +124,9 @@ STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
     'techit/static',
 ]
-
+ # media files configurations
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR /'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
