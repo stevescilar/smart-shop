ca6bba45b4809eb686e0ae4914070f7eb5b4ba58
stevescilar <stevemuambi@gmail.com>
2021-12-23 23:12:54 +0300

    oiling

---
+++ category/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.db.models.fields.files import ImageField
+
+# Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, unique=True)
+    slug = models.CharField(max_length=100, unique=True)
+    description = models.CharField(max_length=255, blank=True) 
+    cat_image = models.ImageField(upload_to='photos/categories/',blank = True)
+
+    def __str__(self):
+        return self.category_name
