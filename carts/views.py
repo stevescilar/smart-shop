4e0c4c0d7bebdae9528e387145c034873c9deaa7
stevescilar <stevemuambi@gmail.com>
2022-01-03 16:03:00 +0300

    update on cart fxns

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import ObjectDoesNotExist
 from django.http.response import HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from store.models import Product
 from .models import Cart,CartItem
 
@@ -34,8 +34,30 @@ def add_cart(request, product_id):
         cart_item.save()
     return redirect('cart')
 
+#remove product from cart
 
-# Create your views here.
+def remove_cart (request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
+
+#remove cart item completely
+
+def remove_cart_item(request,product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
+    return redirect('cart')
+
+#add items to cart
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
@@ -44,6 +66,8 @@ def cart(request, total=0, quantity=0, cart_items=None):
         for cart_item in cart_items: 
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
+        tax = (16 * total )/100
+        grand_total = total + tax
     except ObjectDoesNotExist:
         pass #just ignore
     #create a context list to pass the items to cart template
@@ -51,5 +75,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
         'total' : total,
         'quantity' : quantity,
         'cart_items' : cart_items,
+        'tax' : tax,
+        'grand_total' : grand_total,
         }
     return render (request, 'store/cart.html', context)
