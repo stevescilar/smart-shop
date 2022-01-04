ff606c9af90b89bbc37b11b81dca258103b430e8
stevescilar <stevemuambi@gmail.com>
2022-01-04 10:50:05 +0300

    Day 4 - cart issues fixes

--- store/views.py
+++ store/views.py
@@ -1,7 +1,10 @@
+# from django.http.response import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
 from .models import Product
 from category.models import Category
+from carts.models import CartItem
+from carts.views import _cart_id
 # Create your views here.
 def store(request,category_slug=None):
     #filtering product by category
@@ -29,9 +32,13 @@ def product_detail(request, category_slug, product_slug):
     #show a single product
     try:
         single_product = Product.objects.get(category__slug=category_slug,slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request),product=single_product).exists()
+        # return HttpResponse(in_cart)
+        # exit()
     except Exception as e:
         raise e
     context = {
         'single_product': single_product,
+        'in_cart' : in_cart
     }
     return render(request,'store/product_detail.html',context)
