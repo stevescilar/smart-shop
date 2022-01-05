e850d68d5037dd4b8a5e9af5c277f6a5c61b9c1a
stevescilar <stevemuambi@gmail.com>
2022-01-05 11:49:44 +0300

    more on pagination

--- store/views.py
+++ store/views.py
@@ -14,6 +14,9 @@ def store(request,category_slug=None):
     if category_slug !=None:
         categories = get_object_or_404(Category, slug = category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
+        paginator = Paginator(products, 4)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
         
         #displaying all products
