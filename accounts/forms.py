3afeb8ae43f8eb74bc2ae66761b1e9991a0a902e
stevescilar <stevemuambi@gmail.com>
2022-01-07 17:04:36 +0300

    using Cleanmethod to verify password

--- accounts/forms.py
+++ accounts/forms.py
@@ -12,13 +12,24 @@ class RegistrationForm(forms.ModelForm):
         model =Account
         fields = ['first_name','last_name','phone','email','password']
     
+    def clean(self):
+        cleaned_data =  super(RegistrationForm,self).clean()
+        password  = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Password does not match!"
+            )
+    
+
+
     def __init__(self,*args, **kwargs):
         super(RegistrationForm,self).__init__(*args, **kwargs)
         self.fields['first_name'].widget.attrs['placeholder']='First Name'
         self.fields['last_name'].widget.attrs['placeholder']='Last Name'
         self.fields['phone'].widget.attrs['placeholder']='Phone Number'
         self.fields['email'].widget.attrs['placeholder']='Email Address'
+
         for field in self.fields:
             self.fields[field].widget.attrs['class']='form-control'
-
- 
