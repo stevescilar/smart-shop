1949b11592c3855c94defdaccaae118c49f56101
stevescilar <stevemuambi@gmail.com>
2022-01-07 16:24:24 +0300

    using Cleanmethod to verify password

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
+
+from accounts.models import Account
 from .forms import RegistrationForm
 
-# Create your views here.
+
 def register(request):
-    form  = RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            phone = form.cleaned_data['phone']
+            password = form.cleaned_data['password']
+            username = email.split("@")[0]
+            user = Account.objects.create_user(first_name=first_name,last_name=last_name,username=username,email=email,password=password)
+            user.phone = phone
+            user.save()
+    else:
+        form  = RegistrationForm()
     context = {
         'form':form,
     }
