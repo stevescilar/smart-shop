3e04bb033b22f819389ba65168c098348f8de114
stevescilar <stevemuambi@gmail.com>
2022-01-07 15:56:37 +0300

    build up registration

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,8 +2,23 @@ from django import forms
 from .models import Account
 
 class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder':'Enter Password'
+    }))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder':'Re-Enter Password'
+    }))
     class Meta:
         model =Account
         fields = ['first_name','last_name','phone','email','password']
+    
+    def __init__(self,*args, **kwargs):
+        super(RegistrationForm,self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder']='First Name'
+        self.fields['last_name'].widget.attrs['placeholder']='Last Name'
+        self.fields['phone'].widget.attrs['placeholder']='Phone Number'
+        self.fields['email'].widget.attrs['placeholder']='Email Address'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class']='form-control'
 
  
